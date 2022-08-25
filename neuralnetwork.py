9ca83b6ba7d6a1a3ca1f56c09370a3c257b0a80b
ZeeFcd <ZeeFcd@protonmail.ch>
2022-08-26 01:08:53 +0200

    ReLU and softmax activation function created with forwardpass

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -10,9 +10,37 @@ class Layer_Dense:
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
 
-    # Forward pass
     def forward(self, inputs):
         # Remember input values
         self.inputs = inputs
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+
+    def __init__(self):
+        self.inputs = None
+        self.output = None
+
+    def forward(self, inputs):
+
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+
+    def __init__(self):
+        self.inputs = None
+        self.output = None
+
+    def forward(self, inputs):
+        self.inputs = inputs
+
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+
+        self.output = probabilities

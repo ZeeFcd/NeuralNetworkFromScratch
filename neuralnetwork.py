9dc7fa0777a25b85a8763149eadc3004da8404b4
ZeeFcd <ZeeFcd@protonmail.ch>
2022-08-27 18:08:37 +0200

    added backwardpass everywhere we have a forwardpass

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,10 +1,14 @@
 import numpy as np
 
+
 class Layer_Dense:
 
     # Layer initialization
     def __init__(self, n_inputs, n_neurons):
         # Initialize weights and biases
+        self.dinputs = None
+        self.dbiases = None
+        self.dweights = None
         self.output = None
         self.inputs = None
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
@@ -16,20 +20,38 @@ class Layer_Dense:
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
 
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+
 class Activation_ReLU:
 
     def __init__(self):
+        self.dinputs = None
         self.inputs = None
         self.output = None
 
     def forward(self, inputs):
-
         self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify original variable,
+        # let's make a copy of values first
+        self.dinputs = dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
+
 
 class Activation_Softmax:
 
     def __init__(self):
+        self.dinputs = None
         self.inputs = None
         self.output = None
 
@@ -45,13 +67,30 @@ class Activation_Softmax:
 
         self.output = probabilities
 
+    def backward(self, dvalues):
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                              np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+                                         single_dvalues)
+
+
 # Common loss class
 class Loss:
 
     # Calculates the data and regularization losses
     # given model output and ground truth values
     def calculate(self, output, y):
-
         # Calculate sample losses
         sample_losses = self.forward(output, y)
 
@@ -64,6 +103,9 @@ class Loss:
 
 # Cross-entropy loss
 class Loss_CategoricalCrossentropy(Loss):
+
+    def __init__(self):
+        self.dinputs = None
 
     def forward(self, y_pred, y_true):
 
@@ -81,7 +123,6 @@ class Loss_CategoricalCrossentropy(Loss):
                 range(samples),
                 y_true
             ]
-
         # Mask values - only for one-hot encoded labels
         elif len(y_true.shape) == 2:
             correct_confidences = np.sum(
@@ -92,3 +133,20 @@ class Loss_CategoricalCrossentropy(Loss):
         # Losses
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
